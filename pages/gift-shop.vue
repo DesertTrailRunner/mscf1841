8b55a92984dea214a822d3e2ed88f8d361e077a3
Ben Hoffmann <bk_hoffmann@outlook.com>
2022-09-15 17:46:33 -0400

    added martha to gift shop

--- pages/gift-shop.vue
+++ pages/gift-shop.vue
@@ -34,10 +34,74 @@ export default Vue.extend({
   <div>
     <h1>Gift Shop</h1>
     <p>
-      Browse our gifts and memorabilia for the Monroe Street Cemetery. Purchase online or order by mail. All
-      purchases go toward the <NuxtLink to="/about">Monroe Street Cemetery Foundation activities.</NuxtLink>
+      Browse our gifts and memorabilia for the Monroe Street Cemetery. Purchase
+      online or order by mail. All purchases go toward the
+      <NuxtLink to="/about"
+        >Monroe Street Cemetery Foundation activities.</NuxtLink
+      >
     </p>
     <div class="columns2">
+      <section>
+        <h2>Books</h2>
+        <h4>Get out your reading glasses!</h4>
+        <ImageFigure
+          src="images/gift-shop/book.png"
+          caption="Cemetery History Book"
+          size="medium"
+        />
+        <h3>The Martha Washington Case</h3>
+        <ImageFigure
+          src="images/gift-shop/martha-washington-case.jpg"
+          caption="The Martha Washington Case Book"
+          size="medium"
+        />
+        <h3>Ripped from the Headlines</h3>
+        <p>
+          The eighteen stories included in this book have certain things in
+          common. First the primary person(s) in each story is buried at Monroe
+          Street Cemetery, Cleveland's oldest, west side public cemetery.
+          Second, these stories were all recorded in local newspapers and some
+          of them in national newspapers also. Third, each story deals with the
+          death of one or more people. These stories represent a common problem
+          of comeleries. We see a headstone, we see a name, we see dates, but we
+          don't see the story.
+        </p>
+        <ImageFigure
+          src="images/gift-shop/headlines.jpg"
+          caption="Ripped from the Headlines Book"
+          size="medium"
+        />
+        <p>
+          For you history buffs or if you just want to know more about Monroe
+          Street Cemetery and persons at rest there, we have assembled a
+          collection of fifty brief biographies and a lot of other interesting
+          stuff about Ohio City, the cemetery, Cleveland and life in the
+          Victorian Era.
+        </p>
+        <h3>Cemetery History</h3>
+        <p>
+          The <NuxtLink to="/biographies">biographies</NuxtLink> are stories
+          taken directly from the book. A black and white edition is available
+          for $25.00. Please add $5.00 to your order for S&H.
+        </p>
+      </section>
+
+      <section>
+        <h2>Order online:</h2>
+        <div ref="formEmbed"></div>
+        <h3>Or:</h3>
+        <PrimaryButton
+          label="Download order form"
+          url="/downloads/MSCF-gift-shop-order-form.doc"
+        />
+        <p>
+          Write your selections on the order form, make your check out to Monroe
+          Street Cemetery Foundation and mail it to the address: Monroe Street
+          Cemetery Foundation P.O. BOX 93671, CLEVELAND, OHIO 44101
+        </p>
+        <p>Please allow 4-6 weeks for delivery.</p>
+      </section>
+
       <section>
         <h2>T-Shirts</h2>
         <p>
@@ -57,34 +121,6 @@ export default Vue.extend({
         <ImageFigure
           src="images/gift-shop/MSCF-T-Shirt-back.png"
           caption="T-shirt back"
-          size="medium"
-        />
-
-        <h2>Books</h2>
-        <h4>Get out your reading glasses!</h4>
-        <p>
-          For you history buffs or if you just want to know more about Monroe
-          Street Cemetery and persons at rest there, we have assembled a
-          collection of fifty brief biographies and a lot of other interesting
-          stuff about Ohio City, the cemetery, Cleveland and life in the
-          Victorian Era.
-        </p>
-        <h3>Cemetery History</h3>
-        <p>
-          The <NuxtLink to="/biographies">biographies</NuxtLink> are stories
-          taken directly from the book. A black and white edition is available
-          for $25.00. Please add $5.00 to your order for S&H.
-        </p>
-        <ImageFigure
-          src="images/gift-shop/book.png"
-          caption="Cemetery History Book"
-          size="medium"
-        />
-        <h3>Ripped from the Headlines</h3>
-        <p>The eighteen stories included in this book have certain things in common. First the primary person(s) in each story is buried at Monroe Street Cemetery, Cleveland's oldest, west side public cemetery. Second, these stories were all recorded in local newspapers and some of them in national newspapers also. Third, each story deals with the death of one or more people. These stories represent a common problem of comeleries. We see a headstone, we see a name, we see dates, but we don't see the story.</p>
-        <ImageFigure
-          src="images/gift-shop/headlines.jpg"
-          caption="Ripped from the Headlines Book"
           size="medium"
         />
       </section>
@@ -111,23 +147,6 @@ export default Vue.extend({
           caption="The iron gate at the cemetery entrance archway."
           size="large"
         />
-      </section>
-      <section>
-        <h2>Order online:</h2>
-        <div ref="formEmbed"></div>
-      </section>
-      <section>
-        <h3>Or:</h3>
-        <PrimaryButton
-          label="Download order form"
-          url="/downloads/MSCF-gift-shop-order-form.doc"
-        />
-        <p>
-          Write your selections on the order form, make your check out to Monroe
-          Street Cemetery Foundation and mail it to the address: Monroe Street
-          Cemetery Foundation P.O. BOX 93671, CLEVELAND, OHIO 44101
-        </p>
-        <p>Please allow 4-6 weeks for delivery.</p>
       </section>
     </div>
   </div>
