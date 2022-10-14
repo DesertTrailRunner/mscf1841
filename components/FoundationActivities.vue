96805621d241505635288d0999c8465ca3f050a8
Ben Hoffmann <bk_hoffmann@outlook.com>
2022-10-14 16:13:04 -0400

    activities and regrow promo

--- components/FoundationActivities.vue
+++ components/FoundationActivities.vue
@@ -1,7 +1,7 @@
 <script lang='ts'>
 /**
  * FoundationActivities
- * @date 2022-06-23
+ * @date 2022-10-14
  */
 import Vue, { PropOptions } from "vue";
 
@@ -10,19 +10,21 @@ export default Vue.extend({});
 
 <template>
   <div>
-    <h3>Foundation Activities</h3>
+    <h3>Foundation Activities in 2022</h3>
     <ol>
-      <li>Coordinate maintenance of existing trees</li>
-      <li>Plant new trees and pollinator gardens</li>
-      <li>Plant flowers</li>
+      <li>Plant over a dozen <a href="https://www.instagram.com/p/Cdik64QAK3-/" target="events">new trees in 2022</a></li>
+      <li>Genealogy and history research, including uncovering <NuxtLink to="maps-and-records">burial records prior 1863</NuxtLink></li>
+      <li>Plant <a href="https://www.instagram.com/p/CihwQopAzrR/" target="events">flowers</a> to further beautify the Cemetery</li>
+      <li>Writing <NuxtLink to="gift-shop">new books about Cemetery stories:</NuxtLink> <i>The Martha Washington Case</i> and <i>Ripped from the Headlines</i></li>
+      <li>Event programming to make the Cemetery a neighborhood amenity, such as the <a href="https://www.instagram.com/p/CjiPd_YsKSm/" target="events">Ghost Tour</a>, <a href="https://www.instagram.com/p/CedopUANllw/" target="events">Tree Wedding</a> and <a href="https://regrow-monroe.eventbrite.com/?aff=web" target="events">Regrow Monroe Fundraiser</a></li>
       <li>
-        Coordinate repairs and maintenance to the gatehouse, mausoleums, and
+        Coordinate repairs and maintenance to the <b>wrought iron fencing,</b> gatehouse, mausoleums, and
         monument restoration
       </li>
-      <li>Genealogy and history research</li>
+      <li>Decorating veteran graves with flags for Memorial Day and Veterans Day</li>
+      <li>Coordinate maintenance of existing trees</li>
       <li>Educational initiatives and community engagement</li>
       <li>Pursue arboretum accreditation</li>
-      <li>Event programming to make the Cemetery a neighborhood amenity</li>
     </ol>
   </div>
 </template>
