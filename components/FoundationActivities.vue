e9b5e210a4f8af6d5787a9e7d8be25c33baf6ba9
Ben Hoffmann <bk_hoffmann@outlook.com>
2023-06-14 19:10:34 -0400

    fixed grammar in activities

--- components/FoundationActivities.vue
+++ components/FoundationActivities.vue
@@ -12,25 +12,25 @@ export default Vue.extend({});
   <div>
     <h3>Foundation Activities in 2023</h3>
     <ol>
-      <li>Planting trees</li>
+      <li>Planted 16 trees</li>
       <li>Tree clean-up and weed removal</li>
-      <li><a href="https://www.instagram.com/p/CqbCpLNJX7q/" target="cleanup">Uncovering of hidden graves</a></li>
-      <li><a href="https://www.instagram.com/p/Cs7BouDNxPx/" target="memorial">Decorating veteran graves with flags for Memorial Day</a></li>
+      <li><a href="https://www.instagram.com/p/CqbCpLNJX7q/" target="cleanup">Uncovered hidden graves</a></li>
+      <li><a href="https://www.instagram.com/p/Cs7BouDNxPx/" target="memorial">Decorated veteran graves with flags for Memorial Day</a></li>
     </ol>
     <h3>Foundation Activities in 2022</h3>
     <ol>
       <li><a href="http://www.arbnet.org/morton-register/historic-monroe-street-cemetery-arboretum" target="arbnet">Accredited arboretrum</a></li>
-      <li>Plant over a dozen <a href="https://www.instagram.com/p/Cdik64QAK3-/" target="events">new trees in 2022</a></li>
+      <li>Planted over a dozen <a href="https://www.instagram.com/p/Cdik64QAK3-/" target="events">new trees in 2022</a></li>
       <li>Genealogy and history research, including uncovering <a href="/maps-and-records">burial records prior 1863</a></li>
-      <li>Plant <a href="https://www.instagram.com/p/CihwQopAzrR/" target="events">flowers</a> to further beautify the Cemetery</li>
-      <li>Writing <a href="/gift-shop">new books about Cemetery stories:</a> <i>The Martha Washington Case</i> and <i>Ripped from the Headlines</i></li>
+      <li>Planted <a href="https://www.instagram.com/p/CihwQopAzrR/" target="events">flowers</a> to further beautify the Cemetery</li>
+      <li>Wrote <a href="/gift-shop">new books about Cemetery stories:</a> <i>The Martha Washington Case</i> and <i>Ripped from the Headlines</i></li>
       <li>Event programming to make the Cemetery a neighborhood amenity, such as the <a href="https://www.instagram.com/p/CjiPd_YsKSm/" target="events">Ghost Tour</a>, <a href="https://www.instagram.com/p/CedopUANllw/" target="events">Tree Wedding</a> and <a href="https://regrow-monroe.eventbrite.com/?aff=web" target="events">Regrow Monroe Fundraiser</a></li>
       <li>
-        Coordinate repairs and maintenance to the <b>wrought iron fencing,</b> gatehouse, mausoleums, and
+        Coordinated repairs and maintenance to the <b>wrought iron fencing,</b> gatehouse, mausoleums, and
         monument restoration
       </li>
-      <li>Decorating veteran graves with flags for Memorial Day and Veterans Day</li>
-      <li>Coordinate maintenance of existing trees</li>
+      <li>Decorated veteran graves with flags for Memorial Day and Veterans Day</li>
+      <li>Coordinated maintenance of existing trees</li>
       <li>Educational initiatives and community engagement</li>
     </ol>
   </div>
