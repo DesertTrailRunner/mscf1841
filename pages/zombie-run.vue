3afcad5fc4fc8569ff3eba5055b2aa515cf2f8b0
Ben Hoffmann <bk_hoffmann@outlook.com>
2024-03-21 13:11:04 -0400

    set up zombie race info

---
+++ pages/zombie-run.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ZombieRunAndLurch",
+  layout: "default",
+  head: {
+    title: "Zombie Run and Lurch",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Zombie Run and Lurch",
+      },
+    ],
+  },
+  components: {  },
+});
+</script>
+
+<template>
+  <div>
+    <ImageFigure src="images/zombie/zombie-run-logo.png" size="small"/>
+    <h2>5k run or 1 mile fun walk</h2>
+    <h3>Sunday, September 22, 2024 9:00 am</h3>
+
+    <div class="columns2">
+      <div>
+        <p>Join us for an exciting run or walk through Ohio City's South of Lorain neighborhood. Dress up as a Zombie or put on your scariest outfit.
+        </p> 
+        <p>This fast, flat, shadey course will begin and end in the historic Monroe Street Cemetery and travel through <a href="https://www.clevelandmetroparks.com/parks/visit/parks/ohio-erie-canal-reservation/red-line-greenway" target="_blank">the Redline Greenway.</a> You'll experience two of the largest greenspaces in Cleveland!</p>
+      </div>
+
+      <div>
+        <p>
+            Monroe Street Cemetery is the resting place for local political and business leaders, safety forces, veterans and many others who built Cleveland and its communities. There are more than 34,000 stories in this beautiful 13-acre open space, which is also a refuge for birds and wildlife, and provides tree-covered walking paths for the community to enjoy.
+        </p>
+        <p>The Monroe Street Cemetery Foundation, a 501(c)(3) nonprofit, all volunteer organization was established to preserve and enhance the historic cemetery grounds, trees, headstones, monuments and burial structures.</p>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
