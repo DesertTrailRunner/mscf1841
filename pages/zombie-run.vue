e44f2ba151f292439edc680a80183980d775541f
Ben Hoffmann <bk_hoffmann@outlook.com>
2024-04-05 11:35:19 -0400

    zombie description update

--- pages/zombie-run.vue
+++ pages/zombie-run.vue
@@ -26,16 +26,15 @@ export default Vue.extend({
 
     <div class="columns2">
       <div>
-        <p>Join us for an exciting run or walk through Ohio City's South of Lorain neighborhood. Dress up as a Zombie or put on your scariest outfit.
+        <p>Join us for a unique and exciting run or walk through the historic Monroe Street Cemetery and  <a href="https://www.clevelandmetroparks.com/parks/visit/parks/ohio-erie-canal-reservation/red-line-greenway" target="_blank">Cleveland Metroparks Red Line Greenway</a> in Ohio City's South of Lorain neighborhood. Dress up as a Zombie or put on your scariest outfit!
         </p> 
-        <p>The unique course is fast, flat, and shady. It will begin and end in the historic Monroe Street Cemetery and travel through <a href="https://www.clevelandmetroparks.com/parks/visit/parks/ohio-erie-canal-reservation/red-line-greenway" target="_blank">the Redline Greenway.</a> You'll experience two of the largest greenspaces in Cleveland!</p>
+        <p>The course is fast, flat, and shady. You'll experience two of the largest greenspaces in the area, and be supporting an all-volunteer organization that is actively working to preserve and enhance the historic cemetery grounds, plant and maintain trees, repair headstones, monuments and burial structures.</p>
       </div>
 
       <div>
         <p>
             Monroe Street Cemetery is the resting place for local political and business leaders, safety forces, veterans and many others who built Cleveland and its communities. There are more than 34,000 stories in this beautiful 13-acre open space, which is also a refuge for birds and wildlife, and provides tree-covered walking paths for the community to enjoy.
         </p>
-        <p>The Monroe Street Cemetery Foundation, a 501(c)(3) nonprofit, all volunteer organization was established to preserve and enhance the historic cemetery grounds, trees, headstones, monuments and burial structures.</p>
       </div>
       <div>
         
