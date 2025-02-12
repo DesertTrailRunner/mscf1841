fca0a7b8e5b39493bf6650b249f891541e7486f5
Ben Hoffmann <bk_hoffmann@outlook.com>
2025-02-12 17:49:24 -0500

    updated foundation activities for monument setting

--- components/FoundationActivities.vue
+++ components/FoundationActivities.vue
@@ -12,10 +12,13 @@ export default Vue.extend({});
   <div>
     <h3>Foundation Activities in 2024</h3>
     <ol>
-      <li>Trick-or-Treating in the Cemetery as part of Cleveland History Days</li>
-      <li>Monument setting</li>
-      <li><a href="https://www.instagram.com/p/DAyZNsbpepr/" target="_blank">Coffee &amp; Coffins Classic Hearse Cruise-in</a></li>
-      <li>Filling <a href="https://www.instagram.com/p/C9Pt8rztCG1/?img_index=1" target="_blank">sink-holes in the cemetery</a></li>
+      <li><a href="https://www.instagram.com/p/DCcvANXJuRT/?img_index=1" target="_blank">Monument setting</a></li>
+      <li><a href="https://www.instagram.com/p/DBoRPhlplxN/" target="_blank">Trick-or-Treating in the Cemetery as part
+          of Cleveland History Days</a></li>
+      <li><a href="https://www.instagram.com/p/DAyZNsbpepr/" target="_blank">Coffee &amp; Coffins Classic Hearse
+          Cruise-in</a></li>
+      <li>Filling <a href="https://www.instagram.com/p/C9Pt8rztCG1/?img_index=1" target="_blank">sink-holes in the
+          cemetery</a></li>
       <li>Participated in <a href="https://www.gardenwalkcleveland.org/" target="_blank">GardenWalk Cleveland</a></li>
       <li><a href="https://www.instagram.com/p/C8xa9DZp25b/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
           target="_blank">Voices From The Past - Cleveland History Days Event</a></li>
